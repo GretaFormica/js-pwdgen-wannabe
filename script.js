293fd8c3ed6d1b849878474cbe9724b0280afbb7
GretaFormica <greta.formica@gmail.com>
2023-01-20 16:14:20 +0100

    fix

--- script.js
+++ script.js
@@ -3,4 +3,4 @@ let user_name = prompt('Come ti chiami?');
 let user_surname = prompt('Qual è il tuo cognome?');
 let user_color = prompt('Il tuo colore preferito invece?');
 
-document.getElementById ('pass').InnerHTML= ` ${user_name}  ${user_surname} ${user_color}  23 ` ;
+document.getElementById('pass').innerHTML= ` ${user_name}  ${user_surname}  ${user_color}  23 ` ;
